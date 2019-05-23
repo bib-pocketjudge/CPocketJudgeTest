c575f763760536343e745d1733819c358ed37dca
pbs2h17ale <adrian.ledwinka@edu.bib.de>
2019-05-23 17:31:16 +0200

    Initial commit

---
+++ CPocketJudgeTest/Card.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPocketJudgeTest
+{
+    /// <summary>
+    /// Class that represents a Magic: The Gathering(c) card.
+    /// </summary>
+    class Card
+    {
+        /*
+        {
+          "object": "card",
+          "id": "68c73755-9678-467a-abd5-f8dd1556864e",
+          "oracle_id": "721eb5a2-d7cf-4db0-8013-ef3f596c52a5",
+          "multiverse_ids": [
+            442077
+          ],
+          "mtgo_id": 67090,
+          "mtgo_foil_id": 67091,
+          "tcgplayer_id": 161443,
+          "name": "Doomsday",
+          "lang": "en",
+          "released_at": "2018-03-16",
+          "uri": "https://api.scryfall.com/cards/68c73755-9678-467a-abd5-f8dd1556864e",
+          "scryfall_uri": "https://scryfall.com/card/a25/88/doomsday?utm_source=api",
+          "layout": "normal",
+          "highres_image": true,
+          "image_uris": {
+            "small": "https://img.scryfall.com/cards/small/en/a25/88.jpg?1521726223",
+            "normal": "https://img.scryfall.com/cards/normal/en/a25/88.jpg?1521726223",
+            "large": "https://img.scryfall.com/cards/large/en/a25/88.jpg?1521726223",
+            "png": "https://img.scryfall.com/cards/png/en/a25/88.png?1521726223",
+            "art_crop": "https://img.scryfall.com/cards/art_crop/en/a25/88.jpg?1521726223",
+            "border_crop": "https://img.scryfall.com/cards/border_crop/en/a25/88.jpg?1521726223"
+          },
+          "mana_cost": "{B}{B}{B}",
+          "cmc": 3.0,
+          "type_line": "Sorcery",
+          "oracle_text": "Search your library and graveyard for five cards and exile the rest. Put the chosen cards on top of your library in any order. You lose half your life, rounded up.",
+          "colors": [
+            "B"
+          ],
+          "color_identity": [
+            "B"
+          ],
+          "legalities": {
+            "standard": "not_legal",
+            "future": "not_legal",
+            "frontier": "not_legal",
+            "modern": "not_legal",
+            "legacy": "legal",
+            "pauper": "not_legal",
+            "vintage": "legal",
+            "penny": "not_legal",
+            "commander": "legal",
+            "duel": "legal",
+            "oldschool": "not_legal"
+          },
+          "games": [
+            "mtgo",
+            "paper"
+          ],
+          "reserved": false,
+          "foil": true,
+          "nonfoil": true,
+          "oversized": false,
+          "promo": false,
+          "reprint": true,
+          "set": "a25",
+          "set_name": "Masters 25",
+          "set_uri": "https://api.scryfall.com/sets/41ee6e2f-69b3-4c53-8a8e-960f5e974cfc",
+          "set_search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Aa25&unique=prints",
+          "scryfall_set_uri": "https://scryfall.com/sets/a25?utm_source=api",
+          "rulings_uri": "https://api.scryfall.com/cards/68c73755-9678-467a-abd5-f8dd1556864e/rulings",
+          "prints_search_uri": "https://api.scryfall.com/cards/search?order=released&q=oracleid%3A721eb5a2-d7cf-4db0-8013-ef3f596c52a5&unique=prints",
+          "collector_number": "88",
+          "digital": false,
+          "rarity": "mythic",
+          "watermark": "set",
+          "illustration_id": "f7f9a52f-2ff7-48fd-802f-0a481b0c69fb",
+          "artist": "Noah Bradley",
+          "border_color": "black",
+          "frame": "2015",
+          "full_art": false,
+          "story_spotlight": false,
+          "edhrec_rank": 2875,
+          "prices": {
+            "usd": "2.25",
+            "usd_foil": "9.77",
+            "eur": "1.44",
+            "tix": "0.56"
+          },
+          "related_uris": {
+            "gatherer": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=442077",
+            "tcgplayer_decks": "https://decks.tcgplayer.com/magic/deck/search?contains=Doomsday&page=1&partner=Scryfall&utm_campaign=affiliate&utm_medium=scryfall&utm_source=scryfall",
+            "edhrec": "http://edhrec.com/route/?cc=Doomsday",
+            "mtgtop8": "http://mtgtop8.com/search?MD_check=1&SB_check=1&cards=Doomsday"
+          },
+          "purchase_uris": {
+            "tcgplayer": "https://shop.tcgplayer.com/magic/masters-25/doomsday?partner=Scryfall&utm_campaign=affiliate&utm_medium=scryfall&utm_source=scryfall",
+            "cardmarket": "https://www.cardmarket.com/en/Magic/Products/Singles/Masters-25/Doomsday?referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
+            "cardhoarder": "https://www.cardhoarder.com/cards/67090?affiliate_id=scryfall&ref=card-profile&utm_campaign=affiliate&utm_medium=card&utm_source=scryfall"
+          }
+        }
+        */
+
+        public string oracle_id { get; set; }
+        public string name { get; set; }
+        public imgUris image_uris { get; set; }
+        public string mana_cost { get; set; }
+        public float cmc { get; set; }
+        public string type_line { get; set; }
+        public string oracle_text { get; set; }
+        public Uri rulings_uri { get; set; }
+
+        /// <summary>
+        /// Class that contains urls to different pictures of a Magic: The Gathering(c) card.
+        /// </summary>
+        public class imgUris
+        {
+            public string small { get; set; }
+            public string normal { get; set; }
+            public string large { get; set; }
+            public string png { get; set; }
+            public string art_crop { get; set; }
+            public string border_crop { get; set; }
+
+        }
+    }
+}
---
+++ CPocketJudgeTest/Program.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CPocketJudgeTest
+{
+    /// <summary>
+    /// Small console program that allows the user to fetch information about a Magic: The Gathering(c) card using the Scryfall(c) REST-like API.
+    /// </summary>
+    class Program
+    {
+        #region Parameters
+
+        //initial input parameter
+        public static string ini_input = "";
+
+        public static string search_option = "";
+
+        public static string name = "";
+        #endregion
+
+        #region Program
+
+        static void Main(string[] args)
+        {
+            while (ini_input != "x")
+            {
+                ShowMenu();
+
+                switch (ini_input)
+                {
+                    case "-h":
+                        ShowHelp();
+                        break;
+                    case "-g":
+                        GMenu();
+                        break;
+                    case "-r":
+                        RMenu();
+                        break;
+                    case "-x":
+                        Console.WriteLine("Shutting down...");
+                        System.Environment.Exit(1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="json_string">URL to rulings of a specific card.</param>
+        /// <returns></returns>
+        public static string GetRuling(string json_string)
+        {
+            string ruling_url = "";
+
+            try
+            {
+                var cardJSON = JsonConvert.DeserializeObject<Card>(json_string);
+                Console.WriteLine("Deserialized : \n {0}", cardJSON.ToString());
+                Console.WriteLine("Deserialized : \n {0}", cardJSON.rulings_uri.ToString());
+
+                //Assigns the rulings_uri from a card to the string.
+                ruling_url = cardJSON.rulings_uri.ToString();
+            }
+            catch (Exception deserialise_JSON_ex)
+            {
+                Console.WriteLine("Error : {0}", deserialise_JSON_ex.Source);
+                throw;
+            }
+
+            string responseFromServer = "";
+
+            Console.WriteLine(ruling_url);
+
+            WebRequest request = WebRequest.Create(ruling_url);
+
+            WebResponse response = request.GetResponse();
+
+            Console.WriteLine(" This : " + ((HttpWebResponse)response).StatusDescription);
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.  
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.  
+                responseFromServer = reader.ReadToEnd();
+
+                // Display the content deserialised.  
+                deserialiseJSON_dynamic(responseFromServer);
+            }
+
+            // Close the response.  
+            response.Close();
+
+            return responseFromServer;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="search_option">Specifies a search option.</param>
+        /// <param name="name">Specifies a name format.</param>
+        /// <returns></returns>
+        public static string GetCardJSON(string search_option, string name)
+        {
+            string responseFromServer = "";
+
+            string url = "https://api.scryfall.com/cards/named?" + search_option + "=" + name;
+            Console.WriteLine(url);
+
+            WebRequest request = WebRequest.Create(url);
+
+            WebResponse response = request.GetResponse();
+
+            Console.WriteLine(" This : " + ((HttpWebResponse)response).StatusDescription);
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.  
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.  
+                responseFromServer = reader.ReadToEnd();
+
+            }
+
+            // Display the content.  
+            //serialised
+            //Console.WriteLine(responseFromServer);
+            //deserialised
+            deserialiseJSON_Card(responseFromServer);
+
+            // Close the response.  
+            response.Close();
+            return responseFromServer;
+        }
+
+        #region JSON_functions
+
+        public static void deserialiseJSON_dynamic(string json_string)
+        {
+            try
+            {
+                var cardJSON = JsonConvert.DeserializeObject<dynamic>(json_string);
+                Console.WriteLine("Deserialized : \n {0}", cardJSON.ToString());
+
+                /*Does work...safe to use???
+                Console.WriteLine("Deserialized : \n {0}", cardJSON.oracle_text.ToString());
+                Console.WriteLine("Deserialized : \n {0}", cardJSON.rulings_uri.ToString());
+                */
+            }
+            catch (Exception deserialise_JSON_ex)
+            {
+                Console.WriteLine("Error : {0}", deserialise_JSON_ex.Source);
+                throw;
+            }
+        }
+
+        public static void deserialiseJSON_Card(string json_string)
+        {
+            try
+            {
+                var cardJSON = JsonConvert.DeserializeObject<Card>(json_string);
+                //Console.WriteLine("Deserialized : \n {0}", cardJSON.ToString());
+                Console.WriteLine("Printing card");
+                PrintCard(cardJSON);
+            }
+            catch (Exception deserialise_JSON_ex)
+            {
+                Console.WriteLine("Error : {0}", deserialise_JSON_ex.Source);
+                throw;
+            }
+        }
+
+        public static void deserialiseJSON_Ruling(string json_string)
+        {
+            try
+            {
+                var rulingJSON = JsonConvert.DeserializeObject<Rulings>(json_string);
+                Console.WriteLine("Deserialized : \n {0}", rulingJSON.ToString());
+            }
+            catch (Exception deserialise_JSON_ex)
+            {
+                Console.WriteLine("Error : {0}", deserialise_JSON_ex.Source);
+                throw;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// A function that prints information of a card.
+        /// </summary>
+        /// <param name="card">Class that defines a Magic: The Gathering(c) card.</param>
+        public static void PrintCard(Card card)
+        {
+            Console.WriteLine("Oracle ID   : {0}", card.oracle_id.ToString());
+            Console.WriteLine("Name        : {0}", card.name.ToString());
+            Console.WriteLine("Small img   : {0}", card.image_uris.small.ToString());
+            Console.WriteLine("Normal img  : {0}", card.image_uris.normal.ToString());
+            Console.WriteLine("Large img   : {0}", card.image_uris.large.ToString());
+            Console.WriteLine("Img as png  : {0}", card.image_uris.png.ToString());
+            Console.WriteLine("Art crop    : {0}", card.image_uris.art_crop.ToString());
+            Console.WriteLine("Border crop : {0}", card.image_uris.border_crop.ToString());
+            Console.WriteLine("Mana cost   : {0}", card.mana_cost.ToString());
+            Console.WriteLine("CMC         : {0}", card.cmc.ToString());
+            Console.WriteLine("Type line   : {0}", card.type_line.ToString());
+            Console.WriteLine("Oracle text : {0}", card.oracle_text.ToString());
+            Console.WriteLine("Rulings URI : {0}", card.rulings_uri.ToString());
+        }
+
+        #endregion
+
+        #region Menus
+
+        public static void ShowMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+---------------------------+");
+            Console.WriteLine("|Scryfall API - Menu        |");
+            Console.WriteLine("+---------------------------+");
+            Console.WriteLine("|Param -h : Show help       |");
+            Console.WriteLine("|Param -g : Get card by name|");
+            Console.WriteLine("|Param -r : Print ruling    |");
+            Console.WriteLine("|Param -x : Close           |");
+            Console.WriteLine("+---------------------------+");
+            Console.WriteLine("");
+            Console.Write("Enter : ");
+            ini_input = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        public static string ShowMenuSlim()
+        {
+            Console.WriteLine("+---------------------------+");
+            Console.WriteLine("| -g : Get card by name     |");
+            Console.WriteLine("| -r : Print ruling         |");
+            Console.WriteLine("+---------------------------+");
+            Console.WriteLine("");
+            Console.Write("Enter : ");
+            ini_input = Console.ReadLine();
+            Console.WriteLine();
+            return ini_input;
+        }
+
+        public static void GMenu()
+        {
+            Console.WriteLine("Searching...                                           (see help using -h for more information about search parameters)");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.Write("like : ");
+            search_option = Console.ReadLine();
+            if (search_option == "-h")
+            {
+                search_option = "";
+                name = "";
+                HelpToG_searchOption();
+                Console.WriteLine("\nHopefully your question has been answered\n");
+                GMenu();
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("For...                                                   (see help using -h for more information about name formatting)");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.Write("name : ");
+            name = Console.ReadLine();
+            if (name == "-h")
+            {
+                search_option = "";
+                name = "";
+                HelpToG_nameFormatting();
+                Console.WriteLine("\nHopefully your question has been answered\n");
+                GMenu();
+            }
+            Console.WriteLine();
+            GetCardJSON(search_option.ToLower(), name.ToLower());
+        }
+
+        public static void RMenu()
+        {
+            Console.WriteLine("Searching...                                           (see help using -h for more information about search parameters)");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.Write("like : ");
+            search_option = Console.ReadLine();
+            if (search_option == "-h")
+            {
+                search_option = "";
+                name = "";
+                HelpToG_searchOption();
+                Console.WriteLine("\nHopefully your question has been answered\n");
+                RMenu();
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("For...                                                   (see help using -h for more information about name formatting)");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.Write("name : ");
+            name = Console.ReadLine();
+            if (name == "-h")
+            {
+                search_option = "";
+                name = "";
+                HelpToG_nameFormatting();
+                Console.WriteLine("\nHopefully your question has been answered\n");
+                RMenu();
+            }
+            Console.WriteLine();
+            GetRuling(GetCardJSON(search_option.ToLower(), name.ToLower()));
+        }
+        #endregion
+
+        #region Help
+
+        public static void ShowHelp()
+        {
+            Console.WriteLine("To which operation do you seek help?");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            string ti = ShowMenuSlim();
+
+            switch (ti)
+            {
+                case "-g":
+                    HelpToG();
+                    break;
+                case "-r":
+                    Console.WriteLine("--TODO--");
+                    break;
+                default:
+                    ShowHelp();
+                    break;
+            }
+        }
+
+        public static void HelpToG_searchOption()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Help : Search option: ");
+            Console.WriteLine();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("First you are asked to enter your search parameter\n\nThe options are:\n   exact or fuzzy\n");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("exact : \n   If you provide the exact parameter, a card with that exact name is returned.\n  Otherwise, a 404 Error is returned because no card matches.");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("fuzzy : \n   If you provide the fuzzy parameter and a card name matches that string, then that card is returned.\n   If not, a fuzzy search is executed for your card name.\n   The server allows misspellings and partial words to be provided. For example: jac bele will match Jace Beleren.");
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine();
+        }
+
+        public static void HelpToG_nameFormatting()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Help : Name formatting: ");
+            Console.WriteLine();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("\n   For both exact and fuzzy, card names are case-insensitive and punctuation is optional\n   (you can drop apostrophes and periods etc).\n   For example: fIReBALL is the same as Fireball and smugglers copter is the same as Smuggler's Copter.\n");
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine();
+        }
+
+        public static void HelpToG()
+        {
+            Console.WriteLine("HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP");
+            Console.WriteLine("--");
+            HelpToG_searchOption();
+            HelpToG_nameFormatting();
+            Console.WriteLine("--");
+            Console.WriteLine("HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP_HELP");
+            Console.WriteLine();
+        }
+
+        #endregion
+    }
+}
